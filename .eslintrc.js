4043fdde41b02023c3fdef0a5c21b2b0d483d9d0
Jiho Kim <go2zo.ooo@gmail.com>
2021-06-25 09:58:51 +0900

    chore: config eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,21 @@ module.exports = {
   },
   parserOptions: {
     parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2020,
   },
   extends: [
     '@nuxtjs',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'eslint-config-prettier',
+    // 'plugin:prettier/recommended',
+    // 'plugin:nuxt/recommended',
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-ddebugger': process.env.PRE_COMMIT ? 'error' : 'off',
+    'no-console': process.env.PRE_COMMIT ? ['error', { arrow: ['warn', 'error']}] : 'off',
+    'vue/html-self-closing': 'off',
+    'vue/html-indent': 'off',
+  },
 }
